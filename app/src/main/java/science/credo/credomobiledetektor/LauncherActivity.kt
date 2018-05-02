02a88818ba168c8d1d9a34b9147d9e225777a130
alexcwikla <213axl@gmail.com>
2018-05-02 08:38:52 +0200

    Sign in / up
    Added
    LoginActivity
    ResetPasswordActivity
    SignupActivity

---
+++ app/src/main/java/science/credo/credomobiledetektor/LauncherActivity.kt
@@ -0,0 +1,26 @@
+package science.credo.credomobiledetektor
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val check = PreferenceManager.getDefaultSharedPreferences(this).getString("LOGIN_TOKEN",  null)
+
+        var activityIntent = Intent()
+
+        if(check == null){
+            activityIntent = Intent(this, LoginActivity::class.java)
+        }
+        else{
+            activityIntent = Intent(this, MainActivity::class.java)
+        }
+        startActivity(activityIntent)
+        finish()
+    }
+}
