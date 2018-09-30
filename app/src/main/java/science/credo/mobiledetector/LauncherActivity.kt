54721e79b2f1a07143ea33b25b533b5aeddb4fb1
nkg <nkg753@gmail.com>
2018-09-30 22:50:39 +0200

    Update do Android Studio 3.2 and target API to 28

--- app/src/main/java/science/credo/mobiledetector/LauncherActivity.kt
+++ app/src/main/java/science/credo/mobiledetector/LauncherActivity.kt
@@ -1,10 +1,13 @@
 package science.credo.mobiledetector
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.text.Html
 import android.view.View
 import android.widget.TextView
@@ -13,11 +16,33 @@ import kotlinx.android.synthetic.main.activity_launcher.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 import science.credo.mobiledetector.database.ConfigurationWrapper
 import science.credo.mobiledetector.database.UserInfoWrapper
+import android.content.DialogInterface
+import android.os.Build
+import android.provider.Settings
+import android.support.v7.app.AlertDialog
+
 
 const val REQUEST_MAIN = 1
 const val REQUEST_SIGN = 2
 
 class LauncherActivity : AppCompatActivity() {
+
+    companion object {
+        private val CAMERA_PERMISSIONS = arrayOf(
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+        )
+
+        private val REQUEST_PERMISSION_PHONE_STATE = 1
+        private val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 5469
+    }
 
     var debugClicksCount = 0
     val debugClicksToActivate = 7
@@ -73,9 +98,13 @@ class LauncherActivity : AppCompatActivity() {
 
     override fun onPostResume() {
         super.onPostResume()
-        launchSpecificActivity()
-        debugClicksCount = 0
-        endpoint_input.setText(ConfigurationWrapper(this).endpoint, TextView.BufferType.EDITABLE)
+        if (hasAllPermissionsGranted()) {
+            launchSpecificActivity()
+            debugClicksCount = 0
+            endpoint_input.setText(ConfigurationWrapper(this).endpoint, TextView.BufferType.EDITABLE)
+        } else {
+            requestPermissions()
+        }
     }
 
     override fun onPause() {
@@ -98,6 +127,84 @@ class LauncherActivity : AppCompatActivity() {
             }
         } else if (requestCode == REQUEST_SIGN && resultCode == Activity.RESULT_FIRST_USER) {
             activate_email_message.visibility = View.VISIBLE
+        } else if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Requests permissions necessary to use camera and save pictures.
+     */
+    private fun requestPermissions() {
+        if (shouldShowRationale()) {
+            showExplanation()
+        } else {
+            ActivityCompat.requestPermissions(this, CAMERA_PERMISSIONS, REQUEST_PERMISSION_PHONE_STATE)
+        }
+    }
+
+    /**
+     * Tells whether all the necessary permissions are granted to this app.
+     *
+     * @return True if all the required permissions are granted.
+     */
+    private fun hasAllPermissionsGranted(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (permission in CAMERA_PERMISSIONS) {
+                if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false
+                }
+            }
+
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName"))
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE)
+            }
+
+        }
+
+        return true
+    }
+
+    /**
+     * Gets whether you should show UI with rationale for requesting the permissions.
+     *
+     * @return True if the UI should be shown.
+     */
+    private fun shouldShowRationale(): Boolean {
+        for (permission in CAMERA_PERMISSIONS) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun showExplanation() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+                .setMessage("Permision are needs")
+
+        builder.setPositiveButton(android.R.string.ok) {_, _ -> requestPermissions()}
+        builder.create().show()
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSION_PHONE_STATE -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
