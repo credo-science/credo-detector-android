d15fd5500651f29c93907b99e0ec79c91ca8b3af
alexcwikła <aleks.cwikla@gmail.com>
2020-03-30 23:22:22 +0200

    setup

--- app/src/main/java/science/credo/mobiledetector/login/LoginActivity.kt
+++ app/src/main/java/science/credo/mobiledetector/login/LoginActivity.kt
@@ -2,7 +2,9 @@ package science.credo.mobiledetector.login
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -11,19 +13,29 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import science.credo.mobiledetector.App
+import net.openid.appauth.*
+import org.json.JSONException
 import science.credo.mobiledetector.R
+import science.credo.mobiledetector.App
 import science.credo.mobiledetector.intro.IntroActivity
 import science.credo.mobiledetector.main.MainActivity
+import science.credo.mobiledetector.network.Config
 import science.credo.mobiledetector.network.RestInterface
 import science.credo.mobiledetector.utils.Prefs
 
+
 class LoginActivity : AppCompatActivity() {
+
+    val TAG = "LoginActivity"
+    //        ToDo change
+    val CLIENT_ID = "226751"
+    val REQCODE_AUTH = 100
 
     companion object {
         fun intent(context: Context): Intent {
             return Intent(context, LoginActivity::class.java)
         }
+        private lateinit var authorizationService: AuthorizationService
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,32 +48,48 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, IntroActivity::class.java))
         }
 
-        btRegister.setOnClickListener {
-            addFragment(RegisterFragment.newInstance())
-        }
-
         btSignIn.setOnClickListener {
             addFragment(SignInFragment.newInstance())
         }
 
+        btRegister.setOnClickListener {
+            addFragment(RegisterFragment.newInstance())
+        }
+
+        btSignInViaSciStarter.setOnClickListener {
+            signInViaSciStarter()
+        }
+
+
+        silentLogin(savedInstanceState)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+        if (requestCode == REQCODE_AUTH) {
+            handleAuthorizationResponse(intentData)
+        }
+    }
+
+    fun silentLogin(savedInstanceState: Bundle?){
         val savedLogin = Prefs.get(this, String::class.java, Prefs.Keys.USER_LOGIN)
         val savedPassword = Prefs.get(this, String::class.java, Prefs.Keys.USER_PASSWORD)
         if (savedLogin != null && savedPassword != null) {
-            silentLogin(savedLogin, savedPassword)
+            baseSilentLogin(savedLogin, savedPassword)
         } else {
             containerButtons.visibility = View.VISIBLE
             viewProgress.visibility = View.GONE
         }
     }
 
-    fun silentLogin(login: String, password: String) {
+    fun baseSilentLogin(login: String, password: String) {
         GlobalScope.launch(Dispatchers.Main) {
             delay(1000)
             val result = RestInterface.login(this@LoginActivity, login, password)
             if (result.isSuccess()) {
                 viewProgress.visibility = View.GONE
-                App.token = result.getCastedResponse(LoginResponse::class.java)?.token?:""
-                Prefs.put(this@LoginActivity,App.token,Prefs.Keys.USER_TOKEN)
+                App.token = result.getCastedResponse(LoginResponse::class.java)?.token ?: ""
+                Prefs.put(this@LoginActivity, App.token, Prefs.Keys.USER_TOKEN)
                 startActivity(MainActivity.intent(this@LoginActivity))
                 finish()
             } else {
@@ -78,4 +106,80 @@ class LoginActivity : AppCompatActivity() {
         transaction.commit()
     }
 
+    private fun signInViaSciStarter() {
+        authorizationService = AuthorizationService(this)
+
+        AuthorizationServiceConfiguration.fetchFromIssuer(Uri.parse(Config.SCI_STARTER_DISCOVERY_ENDPOINT_HOST)) { authorizationServiceConfiguration, authorizationException ->
+            if (authorizationServiceConfiguration != null) {
+                val authorizationRequest = AuthorizationRequest.Builder(
+                        authorizationServiceConfiguration,
+                        CLIENT_ID,
+                        ResponseTypeValues.CODE,
+                        Uri.parse(Config.SCI_STARTER_REDIRECT_URI)
+                    )
+                    .setScope(Config.SCI_STARTER_SCOPE)
+                    .build()
+                val intent =
+                    authorizationService.getAuthorizationRequestIntent(authorizationRequest)
+
+                startActivityForResult(intent, REQCODE_AUTH)
+            } else {
+                if (authorizationException != null) {
+                    val m = Throwable().stackTrace[0]
+                    Log.e(TAG, "Exception " + authorizationException.toString())
+                }
+                //                    ToDo some failed message with authorizationException
+            }
+        }
+    }
+
+
+    private fun handleAuthorizationResponse(intentData: Intent?) {
+        if (intentData == null) {
+            val stackTraceElement = Throwable().stackTrace[0]
+            Log.e(TAG, "Error intent call" + stackTraceElement.toString())
+            return
+        }
+
+        val authorizationResponse = AuthorizationResponse.fromIntent(intentData)
+        val authorizationException = AuthorizationException.fromIntent(intentData)
+
+        if (authorizationException != null || authorizationResponse == null) {
+            val stackTraceElement = Throwable().stackTrace[0]
+            Log.e(TAG, "Exception " + authorizationException.toString() + " stackTrace " + stackTraceElement.toString())
+            //                    ToDo some failed message with authorizationException
+            return
+        }
+
+        startCodeExchange(authorizationResponse)
+    }
+
+    private fun startCodeExchange(authorizationResponse: AuthorizationResponse){
+
+
+        authorizationService.performTokenRequest(
+            authorizationResponse.createTokenExchangeRequest()
+        ) { tokenResponse2, authorizationException2 ->
+            if (tokenResponse2 == null) {
+                Log.e(TAG, "Exception " + authorizationException2.toString())
+                //         ToDo msg for user whenAuthorizationFails(authorizationException2)
+            } else{
+                startActivity(MainActivity.intent(this@LoginActivity))
+            }
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        authorizationService.dispose()
+    }
+
+
+//v    discovery
+//v    app wysyla requesta z loginem i haslem do SciStarter. i redirect_url (musi trafic na whiteliste)
+//    On uwierzytelnia, tworzy id_token, przekierowuje do Serwera Credo (on odpytuje tokenem SciStarter)
+//    Serwer odsyla token CREDOWY do aplikacji
+//        1. Ok
+//        2. Konto stworzone, ale defult. Wyswietlamy formularz do zmiany danych request na API_URL /info
+//    ID_TOKEN jest zapisany na serwerze i bedzie uzywany do dodawania punktow
+//    Inny adres email: Jesli user chce powiazac stare konto do SciStartera to kontaktuj
 }
--- app/src/main/java/science/credo/mobiledetector/network/Config.kt
+++ app/src/main/java/science/credo/mobiledetector/network/Config.kt
@@ -2,7 +2,13 @@ package science.credo.mobiledetector.network
 
 object Config{
 
-
     const val API_URL = "https://api.credo.science/api/v2/"
 
+    const val SCI_STARTER_DISCOVERY_ENDPOINT_HOST = "https://scistarter.org/"
+    const val SCI_STARTER_REDIRECT_URI = "science.credo.mobiledetector.scheme:/oauth2redirect"
+    const val SCI_STARTER_SCOPE = "openid profile participation"
+    const val SCI_STARTER_STATIC_AUTHORIZATION_ENDPOINT = "https://scistarter.org/authorize"
+    const val SCI_STARTER_TOKEN_ENDPOINT = "https://scistarter.org/token"
+    const val SCI_STARTER_USERINFO_ENDPOINT = "https://scistarter.org/userinfo"
+//    const val API_URL = "https://api.credo.science/api/v2/" + user + o/login/
 }
